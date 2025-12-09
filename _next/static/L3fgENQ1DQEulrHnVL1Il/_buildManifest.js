ae10adfbc79c75c8141e3b151325df94b79366a7
abrar-debug <hoqueabrar@gmail.com>
2025-12-09 21:18:49 +0200

    Updates

--- _next/static/L3fgENQ1DQEulrHnVL1Il/_buildManifest.js
+++ _next/static/L3fgENQ1DQEulrHnVL1Il/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/abrar-portfolio//_next/:path+",
-        "destination": "/abrar-portfolio/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
